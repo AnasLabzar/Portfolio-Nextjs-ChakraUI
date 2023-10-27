e5cf6d6e846d8b5b01a9a580c72c91e62635663b
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2023-10-27 18:53:00 +0100

    Fixed err: loading dog 3d

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,31 @@
-import { ChakraProvider } from "@chakra-ui/react"
 import Layout from '../components/layouts/main'
-import theme from '../lib/theme'
-import Fonts from "../components/fonts"
-import { AnimatePresence } from "framer-motion"
+import Fonts from '../components/fonts'
+import { AnimatePresence } from 'framer-motion'
+import Chakra from '../components/chakra'
 
+if (typeof window !== 'undefined') {
+  window.history.scrollRestoration = 'manual'
+}
 
-const Website = ({ Component, pageProps, router }) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <AnimatePresence mode='wait' initial={true}>
-                <Layout router={router}>
-                    <Component {...pageProps} key={router.route} />
-                </Layout>
-            </AnimatePresence>
-        </ChakraProvider>
-    )
+function Website({ Component, pageProps, router }) {
+  return (
+    <Chakra cookies={pageProps.cookies}>
+      <Fonts />
+      <Layout router={router}>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+          onExitComplete={() => {
+            if (typeof window !== 'undefined') {
+              window.scrollTo({ top: 0 })
+            }
+          }}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </Chakra>
+  )
 }
 
 export default Website
