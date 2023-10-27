9129cfb33864a3dcd3050333fffad371dbfd31d4
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2023-10-27 18:58:16 +0100

    update main

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,27 +1,46 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
-import Navbar from '../navbar'
-import VoxelDog from '../voxel-dog'
-import NoSsr from '../no-ssr'
+import Footer from '../footer'
+import VoxelDogLoader from '../voxel-dog-loader'
+
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
 
 const Main = ({ children, router }) => {
-    return (
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name="viewport" content='width=device-width, initial-scale=1' />
-                <title>Anas Labzar - Homepage</title>
-            </Head>
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Anas homepage" />
+        <meta name="author" content="Anas Labzar" />
+        <meta name="author" content="craftzcats" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:title" content="Anas Labzar" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@craftzcats" />
+        <meta name="twitter:creator" content="@craftzcats" />
+        <meta property="og:site_name" content="Anas Labzar" />
+        <meta name="og:title" content="Anas Labzar" />
+        <meta property="og:type" content="website" />
+        <title>Anas Labzar - Homepage</title>
+      </Head>
 
-            <Navbar path={router.asPath} />
-            
-            <Container maxW="container.md" pt={14}>
-                <NoSsr>
-                    <VoxelDog />
-                </NoSsr>
-                {children}
-            </Container>
-        </Box>
-    )
+      <NavBar path={router.asPath} />
+
+      <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
+
+        {children}
+
+        <Footer />
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
